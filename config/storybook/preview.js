7b60e3d6b18528b954015394dbdc7676676a83c0
Vladislav Chechulin <vladislav.chechulin@smile2impress.com>
2022-10-27 12:41:15 +0200

    fix jest format files

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,19 +1,19 @@
-import {addDecorator} from "@storybook/react"
-import {StyleDecorator} from "../../src/shared/config/storybook/StyleDecorator"
-import {ThemeDecorator} from "../../src/shared/config/storybook/ThemeDecorator"
-import {RouterDecorator} from '../../src/shared/config/storybook/RouterDecorator';
-import {Theme} from "../../src/app/providers/ThemeProvider"
+import { addDecorator } from "@storybook/react";
+import { StyleDecorator } from "../../src/shared/config/storybook/StyleDecorator";
+import { ThemeDecorator } from "../../src/shared/config/storybook/ThemeDecorator";
+import { RouterDecorator } from "../../src/shared/config/storybook/RouterDecorator";
+import { Theme } from "../../src/app/providers/ThemeProvider";
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
-}
+};
 
-addDecorator(StyleDecorator)
-addDecorator(ThemeDecorator(Theme.LIGHT))
-addDecorator(RouterDecorator)
+addDecorator(StyleDecorator);
+addDecorator(ThemeDecorator(Theme.LIGHT));
+addDecorator(RouterDecorator);
