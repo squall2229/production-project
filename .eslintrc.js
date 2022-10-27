13c14100418e1e5aa0a1d784924f633ed255fc98
Vladislav Chechulin <vladislav.chechulin@smile2impress.com>
2022-10-27 10:43:27 +0200

    fix commit eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,74 +1,91 @@
 module.exports = {
-  root: true,
-  extends: ["plugin:react/recommended", "plugin:@typescript-eslint/recommended", "prettier", "plugin:prettier/recommended", "plugin:i18next/recommended", "plugin:storybook/recommended"],
-  parser: "@typescript-eslint/parser",
-  env: {
-    browser: true,
-    es6: true,
-    jest: true,
-    analyze: true
-  },
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-      arrowFunctions: true
-    }
-  },
-  plugins: ["react", "@typescript-eslint", "prettier", "i18next"],
-  settings: {
-    react: {
-      version: "detect"
+    root: true,
+    extends: [
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+        "plugin:prettier/recommended",
+        "plugin:i18next/recommended",
+        "plugin:storybook/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    env: {
+        browser: true,
+        es6: true,
+        jest: true,
     },
-    "import/resolver": {
-      node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-        paths: ["./src"]
-      }
-    }
-  },
-  rules: {
-    "@typescript-eslint/no-unused-vars": "off",
-    "prettier/prettier": ["error", {
-      tabWidth: 4
-    }],
-    "react/jsx-indent": [2, 4],
-    "i18next/no-literal-string": ["error", {
-      markupOnly: true,
-      ignoreAttribute: ["data-testid"]
-    }],
-    indent: [2, 4],
-    "no-unused-vars": "off",
-    "react/jsx-uses-react": "error",
-    "comma-dangle": "off",
-    "function-paren-newline": "off",
-    "global-require": 0,
-    "import/no-dynamic-require": "off",
-    "no-inner-declarations": "off",
-    "class-methods-use-this": "off",
-    "import/extensions": "off",
-    "import/prefer-default-export": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-var-requires": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/ban-types": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-empty-interface": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "react/prop-types": "off",
-    "react/react-in-jsx-scope": "off",
-    "no-empty-function": "off",
-    "@typescript-eslint/no-empty-function": "off",
-    "react/display-name": "off",
-    "no-console": ["error", {
-      allow: ["warn", "error"]
-    }]
-  },
-  overrides: [{
-    files: ["**/src/**/*.test.{ts, tsx}"],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
+            arrowFunctions: true,
+        },
+    },
+    plugins: ["react", "@typescript-eslint", "prettier", "i18next"],
+    settings: {
+        react: {
+            version: "detect",
+        },
+        "import/resolver": {
+            node: {
+                extensions: [".js", ".jsx", ".ts", ".tsx"],
+                paths: ["./src"],
+            },
+        },
+    },
     rules: {
-      "i18next/no-literal-string": "off"
-    }
-  }]
+        "@typescript-eslint/no-unused-vars": "off",
+        "prettier/prettier": [
+            "error",
+            {
+                tabWidth: 4,
+            },
+        ],
+        "react/jsx-indent": [2, 4],
+        "i18next/no-literal-string": [
+            "error",
+            {
+                markupOnly: true,
+                ignoreAttribute: ["data-testid"],
+            },
+        ],
+        indent: [2, 4],
+        "no-unused-vars": "off",
+        "react/jsx-uses-react": "error",
+        "comma-dangle": "off",
+        "function-paren-newline": "off",
+        "global-require": 0,
+        "import/no-dynamic-require": "off",
+        "no-inner-declarations": "off",
+        "class-methods-use-this": "off",
+        "import/extensions": "off",
+        "import/prefer-default-export": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/ban-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-empty-interface": "off",
+        "@typescript-eslint/ban-ts-comment": "off",
+        "react/prop-types": "off",
+        "react/react-in-jsx-scope": "off",
+        "no-empty-function": "off",
+        "@typescript-eslint/no-empty-function": "off",
+        "react/display-name": "off",
+        "no-console": [
+            "error",
+            {
+                allow: ["warn", "error"],
+            },
+        ],
+    },
+    overrides: [
+        {
+            files: ["**/src/**/*.test.{ts, tsx}"],
+            rules: {
+                "i18next/no-literal-string": "off",
+            },
+        },
+    ],
 };
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -9,27 +9,31 @@ export function buildPlugins({
     paths,
     isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
-        new HtmlWebpackPlugin({
-            template: paths.html,
-        }),
-        new webpack.ProgressPlugin(),
-        new MiniCssExtractPlugin({
-            filename: "css/[name].[contenthash:8].css",
-            chunkFilename: "css/[name].[contenthash:8].css",
-        }),
-        new webpack.DefinePlugin({
-            __IS_DEV__: JSON.stringify(isDev),
-        }),
-        ...(isDev && [
-            new webpack.HotModuleReplacementPlugin(),
-             new ReactRefreshWebpackPlugin({
-                overlay: false,
-            }),
-            new BundleAnalyzerPlugin({
-              openAnalyzer: false,
-            })
-        ]),
-        
+    const plugins = [
+      new HtmlWebpackPlugin({
+        template: paths.html,
+    }),
+      new webpack.ProgressPlugin(),
+      new MiniCssExtractPlugin({
+          filename: "css/[name].[contenthash:8].css",
+          chunkFilename: "css/[name].[contenthash:8].css",
+      }),
+      new webpack.DefinePlugin({
+          __IS_DEV__: JSON.stringify(isDev),
+      }),
     ];
+
+    if (isDev) {
+      plugins.push(
+        new webpack.HotModuleReplacementPlugin(),
+         new ReactRefreshWebpackPlugin({
+            overlay: false,
+        }))
+
+      plugins.push(new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+      }))
+    }
+
+    return plugins
 }
