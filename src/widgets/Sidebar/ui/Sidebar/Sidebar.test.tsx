19a5809dab0fea5ec55cbf2cb2edea6cf78bc19c
Vladislav Chechulin <vladislav.chechulin@smile2impress.com>
2022-10-27 16:26:02 +0200

    fix tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,15 @@
 import { Sidebar } from "./Sidebar";
 import { fireEvent, screen } from "@testing-library/react";
-import { renderWithTranslation } from "shared/lib/tests/renderWithTranslation/renderWithTranslation";
+import { componentRender } from "shared/lib/tests/componentRender/componentRender";
 
 describe("Sidebar", () => {
     test("render", () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId("sidebar")).toBeInTheDocument();
     });
 
     test("toggle", () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId("sidebar-toggle");
         fireEvent.click(toggleBtn);
 
